c8eeddb3cc3d8726bb03bdefcc2b476a3d4dd64e
Alejandro Roman Uc Ek <24470069@campeche.tecnm.mx>
2026-09-09 09:23:17 -0600

    Cuenta de uno en uno

---
+++ ContadorManual.Maui/obj/Debug/net10.0-android/android/src/crc64d2edd89c55714303/MainActivity.java
@@ -0,0 +1,46 @@
+package crc64d2edd89c55714303;
+
+
+public class MainActivity
+	extends crc6488302ad6e9e4df1a.MauiAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"";
+		mono.android.Runtime.register ("ContadorManual.Maui.MainActivity, ContadorManual.Maui", MainActivity.class, __md_methods);
+	}
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class) {
+			mono.android.TypeManager.Activate ("ContadorManual.Maui.MainActivity, ContadorManual.Maui", "", this, new java.lang.Object[] {  });
+		}
+	}
+
+	public MainActivity (int p0)
+	{
+		super (p0);
+		if (getClass () == MainActivity.class) {
+			mono.android.TypeManager.Activate ("ContadorManual.Maui.MainActivity, ContadorManual.Maui", "System.Int32, System.Private.CoreLib", this, new java.lang.Object[] { p0 });
+		}
+	}
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
